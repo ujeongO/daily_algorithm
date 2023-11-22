9071702a8f2f8b3fdb2d6aa8b55387b0aaec0758
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-11-22 09:24:09 +0900

    Time: 2441 ms (32.99%), Space: 17.2 MB (64.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+        isCorrect = False
+        for i in range(1, len(nums)):
+            for j in range(i):
                 if nums[i] + nums[j] == target:
-                    return (i,j)
+                    isCorrect = True
+                    return [i,j]
+                    break
+            # if isCorrect:
+            #     break

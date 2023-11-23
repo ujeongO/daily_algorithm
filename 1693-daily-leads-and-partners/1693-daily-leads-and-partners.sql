afaff8a4b94943237cbccbc4dea9ac67052de6fc
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-11-23 10:46:04 +0900

    Time: 914 ms (86.45%), Space: 0B (100.00%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,14 @@
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM
+    DailySales
+GROUP BY
+    make_name,
+    date_id
+ORDER BY
+    date_id,
+    make_name
+    

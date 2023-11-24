d14962090eee1a53cbb6de8e6c3083dfc2b81bae
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-11-24 11:27:45 +0900

    Time: 58 ms (24.45%), Space: 16.2 MB (54.87%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,26 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {
-            'I': 1,
-            'IV': 4,
-            'V': 5,
-            'IX': 9,
-            'X': 10,
-            'XL': 40,
-            'L': 50,
-            'XC': 90,
-            'C': 100,
-            'CD': 400,
-            'D': 500,
-            'CM': 900,
-            'M': 1000
-        }
-        keys = sorted(d.keys(), reverse=True)
         answer = 0
-        while s:
-            for key in keys:
-                if s.startswith(key):
-                    answer += d[key]
-                    s = s[len(key):]
-                    break
-        return answer
+        sym = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        for i in range(len(s)-1):
+            if sym[s[i]] < sym[s[i+1]]:
+                answer -= sym[s[i]]
+                continue
+            answer += sym[s[i]]
+        return answer + sym[s[-1]]

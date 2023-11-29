6161b9942e8e20325345e42adc0accd3da4780cb
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-11-29 10:05:04 +0900

    Time: 874 ms (33.04%), Space: 16.4 MB (7.13%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.py
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.py
@@ -1,18 +1,14 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         answer = 0
-        for num in range(low, high+1):
-            num = str(num)
-            if len(num)%2 != 0:
+        for i in range(low, high+1):
+            num_str = str(i)
+            # 길이가 홀수일 때 continue
+            if len(num_str)%2 == 1:
                 continue
-            num1 = num[:len(num)//2]
-            num2 = num[len(num)//2:]
-            sum1, sum2 = 0, 0
 
-            for i in range(len(num)//2):
-                sum1 += int(num1[i])
-                sum2 += int(num2[i])
-                
-            if sum1 == sum2:
+            head = num_str[:len(num_str)//2]
+            tail = num_str[len(num_str)//2:]
+            if sum([int(c) for c in head]) == sum([int(c) for c in tail]):
                 answer += 1
         return answer

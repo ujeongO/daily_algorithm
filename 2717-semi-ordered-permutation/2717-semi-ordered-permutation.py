fa12b8b05c96270f3cfc242abc4f7b963f840695
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-11-30 09:47:09 +0900

    Time: 65 ms (94.12%), Space: 16.6 MB (11.46%) - LeetHub

--- 2717-semi-ordered-permutation/2717-semi-ordered-permutation.py
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.py
@@ -3,9 +3,13 @@ class Solution:
         first_index = nums.index(1)
         last_index = nums.index(len(nums))
         print(first_index, last_index)
-        if first_index < last_index:
-            return first_index + (len(nums) -1 -last_index)
-        else:
-            return first_index + (len(nums) -1 -last_index) -1
+        # 1은 맨 앞으로 가야함
+        # 즉, 현재 인덱스 만큼 앞으로 이동하면 됨
+        # n은 맨 뒤로 가야함
+        # 즉, 배열 전체 길이에서 현재 인덱스만큼 빼주면 됨
+        answer = (len(nums) - last_index) + first_index - 1
+        # 이 때, 1이 n보다 뒤에 있으면, 연산을 한번 더 적게 하게 됨
+        if first_index > last_index:
+            answer -= 1
 
-        return 0
+        return answer 

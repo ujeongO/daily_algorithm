5a19a644a189b9d4018569c35dab45364a91b7b9
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-12-04 10:03:23 +0900

    Time: 64 ms (50.00%), Space: 16.6 MB (100.00%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.py
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -1,9 +1,10 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
-        # 양 사이드 제외
-        # 주위보다 큰 수의 인덱스 반환
         answer = []
         for i in range(1, len(mountain)-1):
-            if (mountain[i-1] < mountain[i]) and (mountain[i] > mountain[i+1]):
+            print(mountain[i], mountain[i-1], mountain[i+1])
+            # 앞에 위치한 수보다 크고, 뒤에 위치한 수보다 크다면
+            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+                # answer에 i를 추가한다.
                 answer.append(i)
         return answer

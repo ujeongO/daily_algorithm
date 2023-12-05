43eb124aa22cf9f5fe239d048473d673c5c1adf2
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-12-05 10:07:54 +0900

    Time: 106 ms (7.58%), Space: 16.5 MB (15.76%) - LeetHub

--- 2908-minimum-sum-of-mountain-triplets-i/2908-minimum-sum-of-mountain-triplets-i.py
+++ 2908-minimum-sum-of-mountain-triplets-i/2908-minimum-sum-of-mountain-triplets-i.py
@@ -1,9 +1,16 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
-        answer = float('inf')
+        answer = -1
+        candidates = []
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 for k in range(j+1, len(nums)):
+                    # 가운데 위치한 인덱스의 수가 제일 큰지 비교
+                    # mountain이라면 합을 구해서, answer보다 작다면 지정
+                    # 이 때 -1일 경우 고려
                     if nums[i] < nums[j] and nums[j] > nums[k]:
-                        answer = min(answer, (nums[i]+nums[j]+nums[k]))
-        return answer if answer!=float('inf') else -1
+                        cur_sum = nums[i]+nums[j]+nums[k]
+                        candidates.append(cur_sum)
+        if not candidates:
+            return -1
+        return min(candidates)

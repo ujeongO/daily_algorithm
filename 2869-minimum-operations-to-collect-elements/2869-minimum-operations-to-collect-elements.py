29a582aba2dbc26c32e18da0a747a74ac4c23572
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-12-07 10:02:35 +0900

    Time: 46 ms (50.26%), Space: 16.2 MB (41.78%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.py
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.py
@@ -1,14 +1,12 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        # set
+        # set.issubset
         collection = set()
         target = set([x+1 for x in range(k)])
-        nums = reversed(nums)
-        cnt = 0
-        for num in nums:
-          cnt += 1
-          if num > k:
-            continue
-          collection.add(num)
-          print(collection)
+
+        # operation 구현
+        for i in range(len(nums)):
+          collection.add(nums[len(nums) -1 -i])
           if target.issubset(collection):
-            return cnt
+              return i+1

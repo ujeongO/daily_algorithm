98fb095882d115ba96a1071e5724ad1ef7eb91e1
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-12-13 17:53:56 +0900

    Time: 135 ms (7.43%), Space: 24.2 MB (5.39%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,11 +2,18 @@ from collections import Counter, defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dictionary
-        # key: 알파벳 순으로 정렬한 문자열
-        # value: 해당하는 문자 리스트
-        d = defaultdict(list)
-        for s in strs:
-            d["".join(sorted(s))].append(s)
-        print(d.values())
-        return list(d.values())
+        # dict 정렬 -> 비교
+        # key가 같은 것들 하나로 묶음
+        answer = []
+        ana = collections.defaultdict(list)
+        for string in strs:
+            c_string = Counter(string)
+            l_string = list(c_string.items())
+            l_string = sorted(l_string)
+            # print(l_string)
+            ana[tuple(l_string)].append(string)
+            # print(ana)
+        # print(ana.items())
+        for k, v in ana.items():
+            answer.append(v)
+        return answer

aa721bdf25215168566b08e80cec942657e90bc2
ujeong <80765028+ujeongO@users.noreply.github.com>
2023-12-14 10:03:22 +0900

    Time: 52 ms (74.43%), Space: 16.5 MB (26.89%) - LeetHub

--- 2682-find-the-losers-of-the-circular-game/2682-find-the-losers-of-the-circular-game.py
+++ 2682-find-the-losers-of-the-circular-game/2682-find-the-losers-of-the-circular-game.py
@@ -1,10 +1,15 @@
 class Solution:
     def circularGameLosers(self, n: int, k: int) -> List[int]:
-        cur_loc = 0         # 현재 위치
-        visited = set()     # 폭탄을 가진 사람
-        cnt = 1
-        while cur_loc not in visited:
-            visited.add(cur_loc)
-            cur_loc = (cur_loc + (cnt * k)) % n
+        cur_loc = 0
+        visited = set()
+        visited.add(cur_loc)
+        cnt = 0
+        while True:
             cnt += 1
+            next_loc = (cur_loc + cnt*k) % n
+            if next_loc in visited:
+                break
+            cur_loc = next_loc
+            visited.add(cur_loc)
         return [x+1 for x in range(n) if x not in visited]
+        

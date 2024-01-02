82522eff366ac9840ce32a1fd9577abda202adb3
ujeong <80765028+ujeongO@users.noreply.github.com>
2024-01-02 09:48:33 +0900

    Time: 226 ms (12.24%), Space: 17.5 MB (5.96%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
@@ -3,9 +3,12 @@ class Solution:
         answer = ""
         mid = len(s) // 2
         for i in range(mid):
+            # 두 알파벳 비교, 알파벳 순서상 더 작은 거 선택
             selected = min(s[i], s[len(s) - i - 1])
             answer += selected
+        # s 길이가 홀수일 때: mid 더해주기
         if len(s) % 2:
             return answer + s[mid] + answer[::-1]
+        # s 길이가 짝수일 때: answer에 뒤집은 거 더해주기
         else:
             return answer + answer[::-1]

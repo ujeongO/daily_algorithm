85a4762b3c57267375ccfb1c5e4e04172f6a0ebf
ujeong <80765028+ujeongO@users.noreply.github.com>
2024-01-10 11:19:35 +0900

    Time: 39 ms (60.68%), Space: 17.4 MB (10.68%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
@@ -1,13 +1,12 @@
 class Solution:
     def minimumMoves(self, s: str) -> int:
         answer = 0
-        chars = [c for c in s]
-        for i, c in enumerate(chars):
-            if c == 'X':
-                chars[i] = "O"
-                if i < len(s) - 1:
-                    chars[i+1] = "O"
-                if i < len(s) - 2:
-                    chars[i+2] = "O"
+        i = 0
+        chart = [c for c in s]
+        while i < len(chart):
+            if chart[i] == 'X':
                 answer += 1
+                i += 3
+            else:
+                i += 1
         return answer

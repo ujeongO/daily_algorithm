0975b52a4f8bc72fe7f49b98ada31dfafbaad72c
ujeong <80765028+ujeongO@users.noreply.github.com>
2024-01-10 09:36:20 +0900

    Time: 193 ms (29.59%), Space: 17.4 MB (19.69%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -4,6 +4,5 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] < nums[j]:
-                    diff = nums[j] - nums[i]
-                    answer = max(answer, diff)
+                    answer = max(answer, nums[j] - nums[i])
         return answer

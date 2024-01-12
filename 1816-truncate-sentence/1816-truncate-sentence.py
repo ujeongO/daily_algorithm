93217c3d69797809d0eb1658b19e60d1d8d99d2c
ujeong <80765028+ujeongO@users.noreply.github.com>
2024-01-12 09:37:05 +0900

    Time: 37 ms (64.45%), Space: 17.4 MB (15.79%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,7 +1,3 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        answer = ""
-        list_s = s.split(" ")
-        list_s = list_s[:k]
-        answer = ' '.join(list_s)
-        return answer
+        return ' '.join(s.split(" ")[:k])

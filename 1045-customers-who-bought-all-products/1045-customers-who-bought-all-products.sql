b9326375dd2e2404f8c337395fa666499afe61e6
오유정 <80765028+ujeongO@users.noreply.github.com>
2024-06-12 18:49:31 +0900

    Time: 1452 ms (96.07%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,4 +1,4 @@
 SELECT C.customer_id
 FROM Customer AS C
 GROUP BY C.customer_id
-HAVING COUNT(*) = (SELECT COUNT(*) FROM Product)
+HAVING COUNT(DISTINCT C.product_key) = (SELECT COUNT(*) FROM Product)
